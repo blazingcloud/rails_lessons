5a95ed3ae7a5224b3340e82a00b6fa6a782f0682
Sarah Allen <sarah@ultrasaurus.com>
2010-10-20 12:02:16 -0400

    remove extra save's

--- 1_model/person_spec.rb
+++ 1_model/person_spec.rb
@@ -14,22 +14,19 @@ describe Person do
   
   it 'should save a valid record with first_name and last name' do
     p = Person.new(:first_name => "Eve", :last_name => "Smith")
-    p.save
     p.should be_valid
   end
 
   it 'should not save a record with only first name' do
     p = Person.new(:first_name => "Eve")
-    p.save
     p.should_not be_valid
-    p.errors[:last_name].should == "can't be blank"
+    p.errors[:last_name].should include("can't be blank")
   end
 
   it 'should not save a record with only last name' do
     p = Person.new(:last_name => "Smith")
-    p.save
     p.should_not be_valid
-    p.errors[:first_name].should == "can't be blank"
+    p.errors[:first_name].should include("can't be blank")
   end
 
 end
