ae582835433b3dd9d99029a66af3a6d5b0772745
Renée De Voursney <renee@nird.us>
2012-10-10 18:06:27 -0700

    add the new steps file

---
+++ outside_in_rails3/features/step_definitions/manage_news_steps.rb
@@ -0,0 +1,23 @@
+Given /^I am on the home page$/ do
+    pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see "(.*?)"$/ do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+Given /^I follow "(.*?)"$/ do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+Given /^I fill in "(.*?)" with "(.*?)"$/ do |arg1, arg2|
+    pending # express the regexp above with the code you wish you had
+end
+
+Given /^I press "(.*?)"$/ do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should be on the home page$/ do
+    pending # express the regexp above with the code you wish you had
+end
